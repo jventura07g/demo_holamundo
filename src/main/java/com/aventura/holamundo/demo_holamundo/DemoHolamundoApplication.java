853166b4251cf780f59d4938757b0ddb496a9392
Jose VENTURA <jventura07g@gmail.com>
2024-02-20 16:31:29 -0500

    primera subida de archivos

---
+++ src/main/java/com/aventura/holamundo/demo_holamundo/DemoHolamundoApplication.java
@@ -0,0 +1,13 @@
+package com.aventura.holamundo.demo_holamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHolamundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHolamundoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aventura/holamundo/demo_holamundo/DemoHolamundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aventura.holamundo.demo_holamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHolamundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
